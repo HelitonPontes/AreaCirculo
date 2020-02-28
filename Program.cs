1fe7880d18ba7e9120e7136d4410d4747b5fd5a4
Heliton Pontes <36651605+HelitonPontes@users.noreply.github.com>
2020-02-28 00:13:15 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaCirculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //area = π . raio2
+            double area,raio1;
+            double P = 3.14159;
+            float raio = float.Parse(Console.ReadLine());
+
+            raio1 = double.Parse(raio.ToString("#0.00"));
+
+            area = P * (raio1 * raio1);
+
+            Console.WriteLine("A=" + area.ToString("#0.0000"));
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AreaCirculo
+ URI C#
